fe180943667a3d1f55eb01b8ef7203088b3a9b6c
Eslam Gohar <islamgoher1@gmail.com>
2022-01-05 23:27:30 +0200

    modify a release

--- src/environments/dist/environment.prod.js
+++ src/environments/dist/environment.prod.js
@@ -3,5 +3,5 @@ exports.__esModule = true;
 exports.environment = void 0;
 exports.environment = {
     production: true,
-    baseUrl: 'https://api.themoviedb.org/3'
+    baseUrl: 'https://api.themoviedb.org/3/'
 };
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
 	production: true,
-	baseUrl: 'https://api.themoviedb.org/3'
+	baseUrl: 'https://api.themoviedb.org/3/'
 };
